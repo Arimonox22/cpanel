2f3cbcc05af44e9d13867904a74ca1d8cb9dc7c3
avcoinHub <94029661+avcoinHub@users.noreply.github.com>
2021-12-11 12:06:57 +0700

    Update and rename server.js to sever.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 6000;
-
-app.use(express.static(__dirname + '/public'));
-app.listen(port, () => {
-    console.log(`Blockchain at http://localhost:${port}`)
-});
